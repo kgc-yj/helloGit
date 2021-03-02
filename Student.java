436748ae61cdfb87d32bf777b4e5f87a37d4fbba
kgc <kgc@qq.com>
2021-03-02 16:54:37 +0800

    添加了带参构造方法

--- Student.java
+++ Student.java
@@ -2,4 +2,8 @@ public class Student{
      private int stuNo;
      private String stuName;
      public Student(){}
+     public Student(int stuNo,String stuName){
+      this.stuNo = stuNo;
+      this.stuName = stuName;
+     }
 }
